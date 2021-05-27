513218694552094532d1defcb6576cf8a5447e18
taesoo choi <eldis2000@gmail.com>
2021-05-27 20:42:14 +0900

    many commit

---
+++ scraping/bs2.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bs = BeautifulSoup(html, 'html.parser')
+#print(bs)
+
+#nameList = bs.findAll('span',{'class':'green'})
+#for name in nameList:
+#    print(name.get_text())
+
+nameList = bs.findAll(text = 'the prince')
+print(len(nameList))
